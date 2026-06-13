cb3b82a8f3634a50a16184cd885bdbb75e7275b2
Lucas local <lg.borges12@gmail.com>
2026-06-13 12:31:05 +0200

    png output working, fixed scale bug

--- grid_to_png.cpp
+++ grid_to_png.cpp
@@ -13,21 +13,21 @@ void writeMatrixToPng(
     int scale,          
     const std::string &filename) {
 
-    size_t grid_width{matrix.size()};
-    size_t grid_heigth{matrix.size()};
+    size_t grid_width{matrix.size() * scale};
+    size_t grid_heigth{matrix.size() * scale};
 
     std::vector<uint8_t> pixels(grid_width * grid_heigth * 3);
     // single row vector 
 
-    for (int y = 0; y < grid_heigth; ++y) {
-        for (int x = 0; x < grid_width; ++x) {
+    for (uint32_t y = 0; y < grid_heigth; ++y) {
+        for (uint32_t x = 0; x < grid_width; ++x) {
 
             uint32_t sx = x / scale;
             uint32_t sy = y / scale;
 
             uint8_t bit = matrix[sx][sy];
                 // static_cast<size_t>(sy) * grid_width + sx];
-            uint8_t value = bit ? 0 : 255; // 1 -> black, 0 -> white
+            uint8_t value = bit ? 255 : 0 ; // 0 -> black, 1 -> white
 
             size_t idx = (static_cast<size_t>(y) * grid_width + x) * 3;
 
@@ -39,5 +39,5 @@ void writeMatrixToPng(
 
     std::ofstream out(filename, std::ios::binary);
     TinyPngOut pngout(grid_width, grid_heigth, out);
-    pngout.write(pixels.data(), static_cast<size_t>(grid_width) * grid_heigth);
+    pngout.write(pixels.data(), grid_width * grid_heigth);
     }
